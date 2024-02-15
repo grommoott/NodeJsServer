c16f8ffe402903ae58118e546d361a58d7057ede
Goofy Goober Самый известный олень <118693279+grommoott@users.noreply.github.com>
2024-02-15 20:58:17 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const http = require("http");
+const fs = require("fs");
 
+http.createServer(function(request, response){
+    response.setHeader("type", "html");
+    response.end(fs.readFileSync("index.html"));
+}).listen(7777, "localhost", function(){
+    console.log("Сервер начал свою работу");
+});
