a24b1a9bf7a18590b680548edf9d6fd2c33c2b0b
Goofy Goober Самый известный олень <118693279+grommoott@users.noreply.github.com>
2024-02-15 21:26:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const fs = require("fs");
 http.createServer(function(request, response){
     response.setHeader("type", "html");
     response.end(fs.readFileSync("index.html"));
-}).listen(7777, "localhost", function(){
+}).listen(10000, "0.0.0.0", function(){
     console.log("Сервер начал свою работу");
 });
